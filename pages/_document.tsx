e8da28de88dbbe3d343a6e91b824fd044f98a91b
Yukti Jhawar <yuktimj2002@gmail.com>
2024-04-08 17:08:06 +0530

    notifications PWA

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,9 +4,10 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <link rel="manifest" href="/manifest.json"></link>
+      {/* <link rel="manifest" href="/manifest.json"></link> */}
       <link rel="manifest" href="https://progressier.app/iyMlRYwbIct59hEMG1gG/progressier.json"/>
       <script defer src="https://progressier.app/iyMlRYwbIct59hEMG1gG/script.js"></script>
+     
       <body>
         <Main />
         <NextScript />
